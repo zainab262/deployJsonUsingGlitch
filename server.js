1eea431ac2e221718d72ad563b6a71c25f7ca19d
ABVenu <avenugopalb@gmail.com>
2024-06-07 15:15:20 +0530

    some updation

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
-const port = 3500; // <== You can change the port
+const port = process.env.PORT || 3200; // <== You can change the port
 
 server.use(middlewares);
 server.use(router);
-server.listen(port,()=>{
-    console.log("server started....")
-    console.log("you can use the backend url as",`http://localhost:${port}`)
-});
+server.listen(port);
