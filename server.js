7a8871d207f1d90e139cc142b322ac1a3c29af86
ABVenu <avenugopalb@gmail.com>
2024-06-07 12:25:38 +0530

    added some data

--- server.js
+++ server.js
@@ -6,4 +6,7 @@ const port = process.env.PORT || 3500; // <== You can change the port
 
 server.use(middlewares);
 server.use(router);
-server.listen(port);
+server.listen(port,()=>{
+    console.log("server started....")
+    console.log("you can use the backend url as",`http://localhost:${port}`)
+});
